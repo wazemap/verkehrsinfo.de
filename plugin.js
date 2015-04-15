e6ad2bbc7dca37285e769acfdc97697e7332b825
wazemap <wazemap@yandex.com>
2015-04-15 15:35:31 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,14 @@
+{
+"moduleId" : "wazemap/verkehrsinfo.de",
+"title" : "verkehrsinfo.de",
+"subtitle" : "www.verkehrsinfo.de",
+"backButton" : true,
+"enableGPS" : true,
+"zoomControl" : false,
+"screenLockRot" : false,
+"reload" : true,
+"shouldOverrideUrlLoading1" : "http://www.verkehrsinfo.de",
+"shouldOverrideUrlLoading2" : "https://www.verkehrsinfo.de",
+"loadUrl" : "http://www.verkehrsinfo.de/mobil/mobileList.php?lat=#lat#&lon=#lng#",
+"browserLaunchLink" : "http://www.verkehrsinfo.de/mobil/mobileList.php?lat=#lat#&lon=#lng#"
+}
